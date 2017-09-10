969b5b4f92800cf5d888d49b81a676896f375d4b
tab <tab@0x09.net>
2017-09-10 03:45:34 -0400

    libpng: ensure alpha is stripped when converting from tRNS

--- lib/image.c
+++ lib/image.c
@@ -57,7 +57,10 @@ static unsigned char* read_png(FILE* f, size_t* width, size_t* height, size_t* n
 
 	int bit_depth = png_get_bit_depth(png_ptr,info_ptr), color = png_get_color_type(png_ptr,info_ptr);
 
-	if(png_get_valid(png_ptr,info_ptr,PNG_INFO_tRNS)) png_set_tRNS_to_alpha(png_ptr);
+	if(png_get_valid(png_ptr,info_ptr,PNG_INFO_tRNS)) {
+		png_set_tRNS_to_alpha(png_ptr);
+		color |= PNG_COLOR_MASK_ALPHA;
+	}
 	if(color & PNG_COLOR_MASK_ALPHA) png_set_strip_alpha(png_ptr);
 	if(color == PNG_COLOR_TYPE_PALETTE) png_set_palette_to_rgb(png_ptr);
 	if(color & PNG_COLOR_MASK_COLOR) png_set_rgb_to_gray(png_ptr,1,-1,-1);
