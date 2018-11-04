c4a5327e6b3336903580adfeed35cf5ed01943b0
tab <tab@0x09.net>
2018-11-03 20:23:48 -0400

    Compute sign diffs as needed in original detection method
    Calculating this after the magnitude check means it will be skipped
    most of the time, resulting in a nice speedup.

--- lib/methods.c
+++ lib/methods.c
@@ -55,6 +55,7 @@ static RDError detect_method_magnitude(const coeff* restrict f, size_t length, s
 
 // Initial algorithm. Somewhat more complicated mixture of the previous.
 // Tests for inverted sign, same magnitude, with lower magnitude/zero crossing in between.
+// Confidence value is the same as detect_method_sign, but results not matching the expected magnitudes are suppressed
 static RDError detect_method_original(const coeff* restrict f, size_t length, size_t n, size_t stride, size_t dist, size_t range, double* result, rdint_index* restrict start, rdint_index* restrict end) {
 #ifndef MAG_RANGE
 #define MAG_RANGE range
@@ -64,20 +65,15 @@ static RDError detect_method_original(const coeff* restrict f, size_t length, si
 	if(maxrange*2 >= length)
 		return RDEOK; //can't do anything
 
-	RDError ret = RDEOK;
-	intermediate* sum = NULL; rdint_storage* sign = NULL;
-	if(!(sum = calloc(length,sizeof(*sum)))) { ret = RDENOMEM; goto end; }
-	if(!(sign = calloc(length-range*2,sizeof(*sign)))) { ret = RDENOMEM; goto end; }
+	intermediate* sum = NULL;
+	if(!(sum = calloc(length,sizeof(*sum))))
+		return RDENOMEM;
 
 	*start = maxrange;
 	*end = length-maxrange;
-	for(rdint_index y = 0; y < n; y++) {
+	for(rdint_index y = 0; y < n; y++)
 		for(rdint_index x = 0; x < length; x++)
 			sum[x] += mi(fabs)(f[y*stride+x*dist]);
-		for(rdint_index x = range; x < length-range; x++)
-			for(rdint_index i = 1; i <= range; i++)
-				sign[x-range] += signbit(f[y*stride+x*dist-i*dist]) != signbit(f[y*stride+x*dist+i*dist]);
-	}
 	for(rdint_index x = 0; x < length; x++)
 		sum[x] /= n;
 	for(rdint_index x = maxrange; x < length-maxrange; x++) {
@@ -89,14 +85,16 @@ static RDError detect_method_original(const coeff* restrict f, size_t length, si
 		int leftexp, rightexp, midexp;
 		mi(frexp)(left,&leftexp); mi(frexp)(right,&rightexp); mi(frexp)(mid,&midexp);
 		if((abs(leftexp - rightexp) < 2) && (!mid || MIN(leftexp,rightexp) >= midexp) && (MIN(left,right) > mid) &&
-		   (mi(fabs)(left - right) < mi(fabs)(left - mid) && mi(fabs)(left - right) < mi(fabs)(right - mid)))
-			result[x-*start] += sign[x-range]/(double)(range*n);
+		   (mi(fabs)(left - right) < mi(fabs)(left - mid) && mi(fabs)(left - right) < mi(fabs)(right - mid))) {
+			rdint_storage sign = 0;
+			for(rdint_index y = 0; y < n; y++)
+				for(rdint_index i = 1; i <= range; i++)
+					sign += signbit(f[y*stride+x*dist-i*dist]) != signbit(f[y*stride+x*dist+i*dist]);
+			result[x-*start] += sign/(double)(range*n);
+		}
 	}
 
-end:
-	free(sum);
-	free(sign);
-	return ret;
+	return RDEOK;
 }
 
 // Lightweight version of original method
