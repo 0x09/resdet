50df623a3d141127422ed514a9cadd03163fc76d
tab <tab@0x09.net>
2021-10-16 20:06:11 -0400

    Fix FILE* leak in read_pgm

--- lib/image/pgm.c
+++ lib/image/pgm.c
@@ -6,20 +6,25 @@ static unsigned char* read_pgm(const char* filename, size_t* width, size_t* heig
 	*nimages = 1;
 	unsigned char* image = NULL;
 	FILE* f = strcmp(filename,"-") ? fopen(filename,"r") : stdin;
+	if(!f)
+		return NULL;
 	uint16_t depth;
 	if(
-		f &&
 		fscanf(f,"P5 %zu %zu %" SCNu16,width,height,&depth) == 3 &&
 		fgetc(f) != EOF &&
 		depth < 256 &&
 		*width && *height &&
 		!(*width > PIXEL_MAX / *height) &&
-		(image = malloc(*width * *height)) &&
-		fread(image,1,*width * *height,f) == *width * *height
-	)
-		return image;
-	free(image);
-	return NULL;
+		(image = malloc(*width * *height))
+	) {
+		if(fread(image,1,*width * *height,f) != *width * *height) {
+			free(image);
+			image = NULL;
+		}
+	}
+	if(f != stdin)
+		fclose(f);
+	return image;
 }
 
 struct image_reader resdet_image_reader_pgm = {
