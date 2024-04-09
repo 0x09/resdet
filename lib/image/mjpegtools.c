0x09/resdet

commit 36636cc005680889cb34b0321ca1752a5ee18044

libresdet: Switch to float for input image buffers
This breaking change is motivated by the fact that resdet performs
substantially better (up to +15% confidence with the default method)
when detecting in the colorspace the input was originally resized in.
This allows clients to provide higher precision inputs that have
undergone colorspace conversions.

This also adds portable floatmap support for resdet's built-in image
readers to enable reading higher precision images using the command-
line tool.
This reader supports a series of PFM images for multiple frame input
as well in the form that FFmpeg and ImageMagick both use for writing
PFM sequences.

lib/image/mjpegtools.c
@@ -3,8 +3,9 @@
 #include <mjpegtools/yuv4mpeg.h>
 #include <fcntl.h>
 
-static unsigned char* read_y4m(const char* filename, size_t* width, size_t* height, size_t* nimages) {
+static float* read_y4m(const char* filename, size_t* width, size_t* height, size_t* nimages) {
 	unsigned char* image = NULL,* discard = NULL;
+	float* imagef = NULL;
 	int fd = strcmp(filename,"-") ? open(filename,O_RDONLY) : 0;
 	if(fd < 0)
 		return NULL;
@@ -37,13 +38,18 @@ static unsigned char* read_y4m(const char* filename, size_t* width, size_t* heig
 		}
 	}
 
+	if(!(imagef = malloc(*width * *height * sizeof(*imagef))))
+		goto end;
+	for(size_t i = 0; i < *width * *height; i++)
+		imagef[i] = image[i]/255.f;
+    free(image);
 end:
 	free(discard);
 	y4m_fini_frame_info(&frame);
 	y4m_fini_stream_info(&st);
 	if(fd > 0)
 		close(fd);
-	return image;
+	return imagef;
 }
 
 

include/resdet.h
@@ -52,16 +52,16 @@ RDMethod* resdet_get_method(const char* name);
 
 size_t resdet_default_range(void);
 
-RDError resdet_read_image(const char* filename, const char* mimetype, unsigned char** image, size_t* nimages, size_t* width, size_t* height);
+RDError resdet_read_image(const char* filename, const char* mimetype, float** image, size_t* nimages, size_t* width, size_t* height);
 
 
-RDError resdetect(unsigned char* restrict image, size_t nimages, size_t width, size_t height,
+RDError resdetect(float* image, size_t nimages, size_t width, size_t height,
                   RDResolution** resw, size_t* countw, RDResolution** resh, size_t* counth,
                   RDMethod* method);
 
 RDError resdetect_file(const char* filename, const char* mimetype, RDResolution** resw, size_t* countw, RDResolution** resh, size_t* counth, RDMethod* method);
 
-RDError resdetect_with_params(unsigned char* restrict image, size_t nimages, size_t width, size_t height,
+RDError resdetect_with_params(float* image, size_t nimages, size_t width, size_t height,
                               RDResolution** resw, size_t* countw, RDResolution** resh, size_t* counth,
                               RDMethod* method, size_t range, float threshold);
 

lib/image.c
@@ -31,10 +31,12 @@ static const char* mimetype_from_ext(const char* filename) {
 		return "video/yuv4mpeg";
 	if(!strcasecmp(ext+1,"pgm"))
 		return "image/x-portable-graymap";
+	if(!strcasecmp(ext+1,"pfm"))
+		return "image/x-portable-floatmap";
 	return "";
 }
 
-RDError resdet_read_image(const char* filename, const char* mimetype, unsigned char** image, size_t* nimages, size_t* width, size_t* height) {
+RDError resdet_read_image(const char* filename, const char* mimetype, float** image, size_t* nimages, size_t* width, size_t* height) {
 	*width = *height = *nimages = 0;
 	*image = NULL;
 
@@ -49,6 +51,10 @@ RDError resdet_read_image(const char* filename, const char* mimetype, unsigned c
 	if(!strcmp(c,"image/x-portable-graymap")) {
 		extern struct image_reader resdet_image_reader_pgm;
 		reader = &resdet_image_reader_pgm;
+	}
+	if(!strcmp(c,"image/x-portable-floatmap")) {
+		extern struct image_reader resdet_image_reader_pfm;
+		reader = &resdet_image_reader_pfm;
 	}
 #ifdef HAVE_LIBJPEG
 	else if(!strcmp(c,"image/jpeg")) {

lib/image.h
@@ -6,7 +6,7 @@
 #include "resdet_internal.h"
 
 struct image_reader {
-	unsigned char* (*read)(const char*,size_t*,size_t*,size_t*);
+	float* (*read)(const char*,size_t*,size_t*,size_t*);
 };
 
 #endif

lib/image/libjpeg.c
@@ -9,24 +9,22 @@ static void jerr_emit_message(j_common_ptr cinfo, int msg_level) {}
 static void jerr_error_exit(j_common_ptr cinfo) { longjmp(cinfo->client_data,1); }
 static void jerr_reset_error_mgr(j_common_ptr cinfo) {}
 
-static unsigned char* read_jpeg(const char* filename, size_t* width, size_t* height, size_t* nimages) {
+static float* read_jpeg(const char* filename, size_t* width, size_t* height, size_t* nimages) {
 	FILE* f = strcmp(filename,"-") ? fopen(filename,"r") : stdin;
 	if(!f)
 		return NULL;
 
 	*nimages = 1;
 	unsigned char* image = NULL;
+	float* imagef = NULL;
 	struct jpeg_decompress_struct cinfo;
 	cinfo.err = &(struct jpeg_error_mgr){
 		.error_exit      = jerr_error_exit,
 		.emit_message    = jerr_emit_message,
 		.reset_error_mgr = jerr_reset_error_mgr
 	};
-	if(setjmp(cinfo.client_data = (jmp_buf){0})) {
-		free(image);
-		image = NULL;
+	if(setjmp(cinfo.client_data = (jmp_buf){0}))
 		goto end;
-	}
 
 	jpeg_create_decompress(&cinfo);
 	jpeg_stdio_src(&cinfo,f);
@@ -45,13 +43,18 @@ static unsigned char* read_jpeg(const char* filename, size_t* width, size_t* hei
 			rows[i] = it+i*cinfo.output_width;
 		it += jpeg_read_scanlines(&cinfo,rows,cinfo.rec_outbuf_height) * cinfo.output_width;
 	}
+	if(!(imagef = malloc(*width * *height * sizeof(*imagef))))
+		goto end;
+	for(size_t i = 0; i < *width * *height; i++)
+		imagef[i] = image[i]/255.f;
 finish:
 	jpeg_finish_decompress(&cinfo);
 end:
 	jpeg_destroy_decompress(&cinfo);
+    free(image);
 	if(f != stdin)
 		fclose(f);
-	return image;
+	return imagef;
 }
 
 struct image_reader resdet_image_reader_libjpeg = {

lib/image/libpng.c
@@ -8,13 +8,14 @@
 static void pngerr_error_fn(png_structp png_ptr, png_const_charp error_msg) { png_longjmp(png_ptr,1); }
 static void pngerr_warning_fn(png_structp png_ptr, png_const_charp warning_msg) {}
 
-static unsigned char* read_png(const char* filename, size_t* width, size_t* height, size_t* nimages) {
+static float* read_png(const char* filename, size_t* width, size_t* height, size_t* nimages) {
 	FILE* f = strcmp(filename,"-") ? fopen(filename,"r") : stdin;
 	if(!f)
 		return NULL;
 
 	*nimages = 1;
 	unsigned char* image = NULL;
+	float* imagef = NULL;
 	png_structp png_ptr = NULL;
 	png_infop info_ptr = NULL;
 	unsigned char header[8];
@@ -58,12 +59,19 @@ static unsigned char* read_png(const char* filename, size_t* width, size_t* heig
 	}
 
 	png_read_end(png_ptr, NULL);
+
+	if(!(imagef = malloc(*width * *height * sizeof(*imagef))))
+		goto end;
+	for(size_t i = 0; i < *width * *height; i++)
+		imagef[i] = image[i]/255.f;
+
 end:
 	if(png_ptr)
 		png_destroy_read_struct(&png_ptr,&info_ptr,NULL);
+    free(image);
 	if(f != stdin)
 		fclose(f);
-	return image;
+	return imagef;
 }
 
 struct image_reader resdet_image_reader_libpng = {

lib/image/magickwand.c
@@ -6,20 +6,20 @@
 #include <wand/MagickWand.h>
 #endif
 
-static unsigned char* read_magick(const char* filename, size_t* width, size_t* height, size_t* nimages) {
-	unsigned char* image = NULL;
+static float* read_magick(const char* filename, size_t* width, size_t* height, size_t* nimages) {
+	float* image = NULL;
 	MagickWand* wand = NewMagickWand();
 	if(!MagickReadImage(wand,filename))
 		goto end;
 	*width = MagickGetImageWidth(wand);
 	*height = MagickGetImageHeight(wand);
 	*nimages = MagickGetNumberImages(wand);
-	if(!(*width && *height && *nimages) || (*width > PIXEL_MAX / *height) || (*width * *height > PIXEL_MAX / *nimages) || !(image = malloc(*width * *height * *nimages)))
+	if(!(*width && *height && *nimages) || (*width > PIXEL_MAX / *height) || (*width * *height > PIXEL_MAX / *nimages) || !(image = malloc(sizeof(*image) * *width * *height * *nimages)))
 		goto end;
 	MagickResetIterator(wand);
 	for(size_t i = 0; i < *nimages; i++) {
 		MagickNextImage(wand);
-		MagickExportImagePixels(wand,0,0,*width,*height,"I",CharPixel,image + i * *width* *height);
+		MagickExportImagePixels(wand,0,0,*width,*height,"I",FloatPixel,image + i * *width* *height);
 	}
 end:
 	DestroyMagickWand(wand);

lib/image/pgm.c
@@ -2,9 +2,9 @@
 
 #include <inttypes.h>
 
-static unsigned char* read_pgm(const char* filename, size_t* width, size_t* height, size_t* nimages) {
+static float* read_pgm(const char* filename, size_t* width, size_t* height, size_t* nimages) {
 	*nimages = 1;
-	unsigned char* image = NULL;
+	float* image = NULL;
 	FILE* f = strcmp(filename,"-") ? fopen(filename,"r") : stdin;
 	if(!f)
 		return NULL;
@@ -15,11 +15,16 @@ static unsigned char* read_pgm(const char* filename, size_t* width, size_t* heig
 		depth < 256 &&
 		*width && *height &&
 		!(*width > PIXEL_MAX / *height) &&
-		(image = malloc(*width * *height))
+		(image = malloc(sizeof(*image) * *width * *height))
 	) {
-		if(fread(image,1,*width * *height,f) != *width * *height) {
-			free(image);
-			image = NULL;
+		int c;
+		for(size_t i = 0; i < *width * *height; i++) {
+			if((c = fgetc(f)) == EOF) {
+				free(image);
+				image = NULL;
+				break;
+			}
+			else image[i] = c/(float)depth;
 		}
 	}
 	if(f != stdin)
@@ -27,6 +32,115 @@ static unsigned char* read_pgm(const char* filename, size_t* width, size_t* heig
 	return image;
 }
 
+#define little_endian() (union { int i; char c; }){1}.c
+
+static bool read_pfm_plane_gray(FILE* f, float* image, size_t width, size_t height, float endianness_scale) {
+	if(endianness_scale < 0 == little_endian()) {
+		for(size_t j = 0; j < height; j++)
+			if(fread(image+(height-j-1)*width,sizeof(float),width,f) < width)
+				return false;
+	}
+	else {
+		for(size_t j = 0; j < height; j++)
+			for(size_t i = 0; i < width; i++) {
+				char buf[4];
+				if(fread(buf,4,1,f) < 1)
+					return false;
+				image[(height-j-1)*width+i] = (union { char c[4]; float f; }){{ buf[3], buf[2], buf[1], buf[0] }}.f;
+			}
+	}
+	return true;
+}
+
+static bool read_pfm_plane_rgb(FILE* f, float* image, size_t width, size_t height, float endianness_scale) {
+    // PF is packed RGB
+	if(endianness_scale < 0 == little_endian()) {
+		for(size_t j = 0; j < height; j++)
+			for(size_t i = 0; i < width; i++) {
+				float pixel[3];
+				if(fread(pixel,sizeof(float),3,f) < 3)
+					return false;
+				image[(height-j-1)*width+i] = (pixel[0] + pixel[1] + pixel[2])/3;
+			}
+	}
+	else {
+		for(size_t j = 0; j < height; j++)
+			for(size_t i = 0; i < width; i++) {
+				char buf[3][4];
+				if(fread(buf,4,3,f) < 3)
+					return false;
+				float pixel[3];
+				for(int c = 0; c < 3; c++)
+					pixel[c] = (union { char c[4]; float f; }){{ buf[c][3], buf[c][2], buf[c][1], buf[c][0] }}.f;
+				image[(height-j-1)*width+i] = (pixel[0] + pixel[1] + pixel[2])/3;
+			}
+	}
+	return true;
+}
+
+static bool read_pfm_plane(FILE* f, float* image, size_t width, size_t height, float endianness_scale, char format) {
+	return format == 'f' ? read_pfm_plane_gray(f,image,width,height,endianness_scale) : read_pfm_plane_rgb(f,image,width,height,endianness_scale);
+}
+
+static float* read_pfm(const char* filename, size_t* width, size_t* height, size_t* nimages) {
+	*nimages = 1;
+	float* image = NULL;
+	FILE* f = strcmp(filename,"-") ? fopen(filename,"r") : stdin;
+	if(!f)
+		return NULL;
+	float endianness_scale;
+	char format;
+	if(!(
+	   fscanf(f,"P%c %zu %zu %f\n",&format,width,height,&endianness_scale) == 4 &&
+	   (format == 'f' || format == 'F') &&
+	   *width && *height &&
+	   !(*width > PIXEL_MAX / *height) &&
+	   (image = malloc(sizeof(*image) * *width * *height))
+	))
+		goto end;
+
+	if(!read_pfm_plane(f,image,*width,*height,endianness_scale,format)) {
+        free(image);
+		image = NULL;
+		goto end;
+	}
+
+	size_t next_width, next_height;
+	float next_endianness_scale;
+	char next_format;
+	int next_char;
+	float* next_image = NULL;
+	while(
+	    (next_char = fgetc(f)) == 'P' &&
+	    fscanf(f,"%c %zu %zu %f\n",&next_format,&next_width,&next_height,&next_endianness_scale) == 4 &&
+	    (next_format == 'f' || next_format == 'F') &&
+	    next_width == *width && next_height == *height && next_endianness_scale == endianness_scale &&
+	    (next_image = realloc(image,sizeof(*image) * *width * *height * ++*nimages))
+	) {
+		if(!next_image) {
+            free(image);
+			image = NULL;
+			goto end;
+		}
+		image = next_image;
+		if(!read_pfm_plane(f,image+*width * *height * (*nimages-1),*width,*height,endianness_scale,next_format))
+			break;
+	}
+
+	if(next_char != EOF) {
+		free(image);
+		image = NULL;
+	}
+
+end:
+	if(f != stdin)
+		fclose(f);
+	return image;
+}
+
 struct image_reader resdet_image_reader_pgm = {
 	.read = read_pgm
+};
+struct image_reader resdet_image_reader_pfm = {
+	.read = read_pfm
 };

commit b95b56a1de3fe8dbcc1dd4e66c7fab30239e9762

Fix reading of multiple y4m frames
Broken in 36636cc005680889cb34b0321ca1752a5ee18044 which only allocates
and copies one frame of a y4m series back to the library, resulting in
resdet later attempting to read past the end of the provided buffer.

lib/image/mjpegtools.c
@@ -38,9 +38,9 @@ static float* read_y4m(const char* filename, size_t* width, size_t* height, size
 		}
 	}
 
-	if(!(imagef = malloc(*width * *height * sizeof(*imagef))))
+	if(!(imagef = malloc(*width * *height * *nimages * sizeof(*imagef))))
 		goto end;
-	for(size_t i = 0; i < *width * *height; i++)
+	for(size_t i = 0; i < *width * *height * *nimages; i++)
 		imagef[i] = image[i]/255.f;
     free(image);
 end: