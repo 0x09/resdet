e05cf97961f9e87e5338a91ad125340d93b120ff
tab <tab@0x09.net>
2025-07-18 14:59:31 -0400

    visualize.sh: add option to set line thickness
    `-t` can now be used to set the thickness (ImageMagick strokewidth) of
    the highlight lines.

--- src/visualize.sh
+++ src/visualize.sh
@@ -19,14 +19,15 @@ if ! command -v magick > /dev/null; then
 fi
 
 usage() {
-	echo "usage: visualize.sh [-l <line_length> -c <line_color> -r <resdet_opts> -s <spec_opts>] image output" 1>&2
+	echo "usage: visualize.sh [-l <line_length> -c <line_color> -t <line_thickness> -r <resdet_opts> -s <spec_opts>] image output" 1>&2
 	exit 2
 }
 
 help() {
-	printf "usage: visualize.sh [-l <line_length> -c <line_color> -r <resdet_opts> -s <spec_opts>] image output\n\
+	printf "usage: visualize.sh [-l <line_length> -c <line_color> -t <line_thickness> -r <resdet_opts> -s <spec_opts>] image output\n\
   -l <line_length>  Highlight line length in pixels. Default: 50\n\
   -c <line_color>   Highlight line color. Default: green\n\
+  -t <thickness>    Highlight line thickness. Default: 1\n\
   -r <resdet_opts>  Quoted option string to pass to resdet\n\
   -s <spec_opts>    Quoted option string to pass to spec\n"
 	exit 0
@@ -34,10 +35,12 @@ help() {
 
 line_length=50
 line_color=green
-while getopts ":l:c:r:s:h" opt; do
+line_thickness=1
+while getopts ":l:c:t:r:s:h" opt; do
 	case $opt in
 		l) line_length=$OPTARG;;
 		c) line_color=$OPTARG;;
+		t) line_thickness=$OPTARG;;
 		r) resdet_opts=$OPTARG;;
 		s) spec_opts=$OPTARG;;
 		h) help;;
@@ -55,7 +58,7 @@ output=$2
 read full_width full_height < <(identify -ping -format '%w %h\n' "$image")
 read detected_width detected_height < <(resdet $resdet_opts -v 1 -- "$image")
 
-spec $spec_opts -- "$image" - | magick - -stroke "$line_color" -draw \
+spec $spec_opts -- "$image" - | magick - -stroke "$line_color" -strokewidth "$line_thickness" -draw \
 	"line $((full_width-line_length)),$detected_height $full_width,$detected_height \
 	 line $detected_width,$((full_height-line_length)) $detected_width,$full_height" \
 	"$output"
