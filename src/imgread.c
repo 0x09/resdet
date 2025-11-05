a67603f338124e409927e8aab9e9316d8df7a928
tab <tab@0x09.net>
2025-11-05 18:30:56 -0500

    imgread: fix missing line break in usage text

--- src/imgread.c
+++ src/imgread.c
@@ -29,7 +29,7 @@
 
 int main(int argc, char** argv) {
 	if(argc < 2) {
-		fprintf(stderr,"Usage: %s input_image output.pfm", argv[0]);
+		fprintf(stderr,"Usage: %s input_image output.pfm\n", argv[0]);
 		return 1;
 	}
 
