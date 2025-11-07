0x09/resdet

commit 534dc501b6c6120830303bc2aefa972f5ecb4010

pgm reader: fix error return in case of realloc failure

lib/image/pgm.c
@@ -116,17 +116,12 @@ static float* read_pfm(const char* filename, size_t* width, size_t* height, size
 	    !resdet_dims_exceed_limit(*width,*height,*nimages,*image) &&
 	    (next_image = realloc(image,sizeof(*image) * *width * *height * ++*nimages))
 	) {
-		if(!next_image) {
-            free(image);
-			image = NULL;
-			goto end;
-		}
 		image = next_image;
 		if(!read_pfm_plane(f,image+*width * *height * (*nimages-1),*width,*height,endianness_scale,next_format))
 			break;
 	}
 
-	if(next_char != EOF) {
+	if(!next_image || next_char != EOF) {
 		free(image);
 		image = NULL;
 	}

commit e34c7b5f86094f7c6d45eaa6b2ce5de168b0de28

pfm reader: fix reading single frame PFM images
This fixes a regression introduced in
534dc501b6c6120830303bc2aefa972f5ecb4010. The additional check for
realloc failure here is not actually necessary as next_char cannot be
EOF in this case.

lib/image/pgm.c
@@ -121,7 +121,7 @@ static float* read_pfm(const char* filename, size_t* width, size_t* height, size
 			break;
 	}
 
-	if(!next_image || next_char != EOF) {
+	if(next_char != EOF) {
 		free(image);
 		image = NULL;
 	}