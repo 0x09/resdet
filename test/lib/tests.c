4d15fe34d11dabb10a35ba522c7f7a860df9f3f9
tab <tab@0x09.net>
2026-02-03 17:20:44 -0500

    tests: support filtering and skipping tests in test_libresdet
    This now accepts a pattern argument to run only the matching tests, and
    supports an `-s <pattern>` option to skip all tests matching that
    pattern.
    -s may be used together with a test pattern to filter down the set of
    matching tests.

--- test/lib/tests.c
+++ test/lib/tests.c
@@ -1,10 +1,26 @@
 #include <stddef.h>
 #include <setjmp.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
 
 #include <cmocka.h>
 
 int main(int argc, char* argv[]) {
+	int c;
+	while((c = getopt(argc,argv,"s:")) != -1) {
+		switch(c) {
+			case 's':
+				cmocka_set_skip_filter(argv[2]);
+				break;
+			default:
+				fprintf(stderr,"usage: %s [-s test_pattern] [test_pattern]\n",argv[0]);
+				return 1;
+		}
+	}
+	if(argc > optind)
+		cmocka_set_test_filter(argv[optind]);
+
 	int ret = 0;
 
 #include "tests_main.c"
