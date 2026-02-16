97c014984584229a0b6befc513e05bb62a20096b
tab <tab@0x09.net>
2026-02-16 10:29:18 -0500

    libjpeg reader: remove redundant setting of out error
    This is always initialized to RDEOK.

--- lib/image/libjpeg.c
+++ lib/image/libjpeg.c
@@ -108,7 +108,6 @@ end:
 }
 
 static bool libjpeg_reader_seek_frame(void* reader_ctx, uint64_t offset, void(*progress)(void*,uint64_t), void* progress_ctx, size_t width, size_t height, RDError* error) {
-	*error = RDEOK;
 	struct libjpeg_context* ctx = reader_ctx;
 	bool ret = !offset || !(ctx->eof || offset > 1);
 	if(offset && !ctx->eof) {
